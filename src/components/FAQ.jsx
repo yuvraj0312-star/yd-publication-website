9ec167bd235cd4feec48953ed11c4dc3949ccbc2
yuvraj0312-star <yuvrajdivte7@gmail.com>
2026-06-20 00:31:13 +0530

    Update FAQ 2.jsx

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -5,10 +5,10 @@ export default function FAQ({ faqs = [] }) {
   const [open, setOpen] = useState(null);
   
   const fallbackFaqs =[
-    { question: 'Are these updated for the 2025-26 syllabus?', answer: 'Yes, all our summary books and past paper solutions are strictly based on the latest Gujarat University syllabus for the 2024-25 academic year.' },
-    { question: 'How do I access the PDF after payment?', answer: "Once your payment is verified, the content will be instantly unlocked in your dashboard. You can access notes till its validity." },
-    { question: 'Can I print these notes?', answer: 'Our notes are designed for digital reading within the YD App to prevent unauthorized distribution. Printing is currently disabled to protect our original content.' },
-    { question: "How do I pay?", answer: 'While UPI is our preferred method' }
+    { question: 'Are these books as per NEP syllabus?', answer: 'Yes, all our books, summary books and past paper are strictly based on the latest Gujarat University syllabus.' },
+    { question: 'How do I access the PDF after payment?', answer: "Once your payment is verified, the content will be instantly unlocked in your mobile app. You can access notes till its validity." },
+    { question: 'Can I print these notes?', answer: 'No, Our notes are designed for digital reading within the YD App to prevent unauthorized distribution. Printing is currently disabled to protect our original content.' },
+    { question: "How can I buy and pay?", answer: 'You can visit in mobile app than choose your specific semester plan or all semester plan and pay via UPI.' }
   ];
 
   const data = faqs.length ? faqs : fallbackFaqs;
